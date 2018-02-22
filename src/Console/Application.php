bdfa9483c3fc10240ec3471e773db16b0d09eb1b
pxgamer <owzie123@gmail.com>
2018-02-22 16:42:12 +0000

    Add application config class

---
+++ src/Console/Application.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Arionum\LightWalletCLI\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+
+/**
+ * Class Application
+ */
+class Application extends BaseApplication
+{
+    const NAME = 'Arionum Light Wallet';
+    const VERSION = '@git-version@';
+
+    /**
+     * Application constructor.
+     *
+     * @param null $name
+     * @param null $version
+     */
+    public function __construct($name = null, $version = null)
+    {
+        parent::__construct(
+            $name ?: static::NAME,
+            $version ?: (static::VERSION === '@'.'git-version@' ? 'source' : static::VERSION)
+        );
+    }
+
+    /**
+     * @return array|\Symfony\Component\Console\Command\Command[]
+     */
+    protected function getDefaultCommands()
+    {
+        $commands = parent::getDefaultCommands();
+
+        return $commands;
+    }
+}
