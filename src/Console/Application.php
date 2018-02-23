9d8b34874dea7687046f6c811b63befb8678bbbc
pxgamer <owzie123@gmail.com>
2018-02-23 15:02:02 +0000

    Create a new Generate command

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -38,6 +38,7 @@ class Application extends BaseApplication
         $commands[] = new Commands\DecryptCommand();
         $commands[] = new Commands\EncryptCommand();
         $commands[] = new Commands\ExportCommand();
+        $commands[] = new Commands\GenerateCommand();
         $commands[] = new Commands\SendCommand();
         $commands[] = new Commands\TransactionCommand();
         $commands[] = new Commands\TransactionsCommand();
