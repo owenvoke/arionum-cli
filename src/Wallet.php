ed21eb94e0a270b26d7248f2e6e4a12d248e7e3b
pxgamer <owzie123@gmail.com>
2018-02-23 11:38:45 +0000

    Add a Wallet class for dealing with wallet files

---
+++ src/Wallet.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace pxgamer\Arionum;
+
+use StephenHill\Base58;
+
+/**
+ * Class Wallet
+ */
+class Wallet
+{
+    /**
+     * The default wallet file name.
+     */
+    const WALLET_NAME = 'wallet.aro';
+
+    /**
+     * @var string
+     */
+    private $path;
+    /**
+     * @var bool
+     */
+    private $exists;
+    /**
+     * @var bool|string
+     */
+    private $rawData;
+    /**
+     * @var string
+     */
+    private $address;
+    /**
+     * @var string
+     */
+    private $publicKey;
+    /**
+     * @var string
+     */
+    private $privateKey;
+
+    /**
+     * Wallet constructor.
+     * @param string $path
+     */
+    public function __construct(string $path = self::WALLET_NAME)
+    {
+        $this->path = $path;
+        $this->exists = file_exists($this->path);
+
+        if ($this->exists) {
+            $this->rawData = file_get_contents($this->path);
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEncrypted()
+    {
+        return substr($this->rawData, 0, 7) !== 'arionum';
+    }
+
+    /**
+     * @param string $password
+     */
+    public function decrypt(string $password)
+    {
+        $decodedData = base64_decode($this->rawData);
+        $iv = substr($decodedData, 0, 16);
+        $enc = substr($decodedData, 16);
+        $hashedPassword = substr(hash('sha256', $password, true), 0, 32);
+        $decrypted = openssl_decrypt(base64_decode($enc), 'aes-256-cbc', $hashedPassword, OPENSSL_RAW_DATA, $iv);
+
+        if (substr($decrypted, 0, 7) == 'arionum') {
+            $this->rawData = $decrypted;
+        }
+    }
+
+    /**
+     *
+     */
+    public function decode()
+    {
+        if (!$this->isEncrypted()) {
+            $decoded = explode(":", $this->rawData);
+
+            $this->publicKey = $decoded[2];
+            $this->privateKey = $decoded[1];
+            $this->address = $this->getAddressFromPublicKey();
+        }
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    private function getAddressFromPublicKey()
+    {
+        $hash = $this->publicKey;
+
+        for ($i = 0; $i < 9; $i++) {
+            $hash = hash('sha512', $hash, true);
+        }
+
+        return (new Base58())->encode($hash);
+    }
+
+    /**
+     * @param string $address
+     * @return bool
+     */
+    public function validAddress(string $address = null): bool
+    {
+        $address = $address ?? $this->address;
+
+        return preg_match('/^[a-z0-9]+$/i', $address);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublicKey()
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrivateKey()
+    {
+        return $this->privateKey;
+    }
+
+    /**
+     * @return bool
+     */
+    public function exists()
+    {
+        return $this->exists;
+    }
+}
