5608b46cc2d6e8467f19ab33e619ca73450ec81f
pxgamer <owzie123@gmail.com>
2018-11-08 17:19:37 +0000

    Use self instead of class name for Wallet

--- src/Wallet.php
+++ src/Wallet.php
@@ -93,7 +93,7 @@ final class Wallet
 
         $publicKey = $this->pem2coin($pub['key']);
 
-        if (strlen($privateKey) < Wallet::MIN_KEY_LENGTH || strlen($publicKey) < Wallet::MIN_KEY_LENGTH) {
+        if (strlen($privateKey) < self::MIN_KEY_LENGTH || strlen($publicKey) < self::MIN_KEY_LENGTH) {
             throw new \Exception('Failed to create the EC key pair. Please check the openssl binaries.');
         }
 
