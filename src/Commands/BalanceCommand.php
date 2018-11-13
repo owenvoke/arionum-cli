aa7f9a6f27284611eb536c421241fa2ff324ebad
pxgamer <owzie123@gmail.com>
2018-11-13 22:23:53 +0000

    Update Balance try/catch to catch ArionumException

--- src/Commands/BalanceCommand.php
+++ src/Commands/BalanceCommand.php
@@ -38,20 +38,29 @@ final class BalanceCommand extends BaseCommand
     {
         parent::execute($input, $output);
 
-        if ($address = $input->getArgument('address')) {
-            $output->writeln('Checking balance of the specified address: '.$address);
-
-            if (!$this->wallet->validAddress($address)) {
-                throw new ArionumException('Invalid address format provided.');
-            }
-        }
-
         try {
-            $result = $this->arionumClient->getBalance($address ?? $this->wallet->getAddress());
+            if ($address = $input->getArgument('address')) {
+                $this->checkAddressValidity($address);
 
-            $output->writeln('Balance: '.$result);
-        } catch (ApiException $exception) {
+                $output->writeln('Checking balance for: '.$address);
+            }
+
+            $balance = $this->arionumClient->getBalance($address ?? $this->wallet->getAddress());
+
+            $output->writeln('Balance: '.$balance);
+        } catch (ApiException|ArionumException $exception) {
             $output->writeln('<fg=red>'.$exception->getMessage().'</>');
+        }
+    }
+
+    /**
+     * @param string $address
+     * @throws ArionumException
+     */
+    private function checkAddressValidity(string $address): void
+    {
+        if (!$this->wallet->validAddress($address)) {
+            throw new ArionumException('Invalid address format provided.');
         }
     }
 }
